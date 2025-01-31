bfc1998d662245e2e40e5c15f9bbbac7d83c543f
aqashyy <ashiqmuhammedep@gmail.com>
2025-01-31 16:46:02 +0530

    Auth routes setup completed

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,13 @@
 <?php
 
+use App\Http\Controllers\Auth\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+Route::prefix('auth')->controller(AuthController::class)->group(function () {
+
+    Route::post('register','register');
+    Route::post('login','login');
+    Route::get('logout','logout')->middleware('auth:sanctum');
+
+});
